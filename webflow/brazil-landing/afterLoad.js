228be90bf61365b0459d65ceb8ca268a90da8958
Fran-Gonzalez <fran.gonzalez1994@gmail.com>
2022-10-20 09:27:00 -0300

    Move get user data to afterLoad.js

--- webflow/brazil-landing/afterLoad.js
+++ webflow/brazil-landing/afterLoad.js
@@ -1,10 +1,71 @@
 // Should load script on end of <body></body>
-// to ensure userData is set on window => from beforeLoad.js
+const getAuthenticationToken = async () => {
+  const cookieAuthToken = await cookieStore.get("auth._token.local");
+
+  const authenticationToken = cookieAuthToken?.value?.startsWith("Bearer")
+    ? decodeURIComponent(cookieAuthToken.value)
+    : undefined;
+
+  return authenticationToken;
+};
+
+const getUserData = async (locale) => {
+  const authToken = await getAuthenticationToken();
+  if (!authToken) {
+    return null;
+  }
+
+  let response;
+  try {
+    response = await fetch("https://gateway.filadd.com/api/user/me/", {
+      method: "GET",
+      headers: {
+        Authorization: authToken,
+        "X-Locale": locale,
+      },
+    });
+  } catch (error) {
+    console.error("Failed to get user data, received error:", error);
+    return null;
+  }
+
+  if (response.status !== 200) {
+    console.error("Failed to get user data, received status:", response.status);
+    return null;
+  }
+
+  const data = await response.json();
+
+  return data.user;
+};
+
 const logoutButton = document.querySelector("#log-out");
 const alreadyUserButton = document.querySelector("#already-user");
 
-if (window.userData) {
-  logoutButton.style.display = "inline-block";
-} else {
-  alreadyUserButton.style.display = "inline-block";
-}
+const displayAuthButtons = (userData) => {
+  if (userData) {
+    logoutButton.style.display = "inline-block";
+  } else {
+    alreadyUserButton.style.display = "inline-block";
+  }
+};
+
+logoutButton.addEventListener("click", async () => {
+  await cookieStore.delete("auth._token.local");
+});
+
+getUserData("BR").then((userData) => {
+  window.userData = userData;
+
+  if (userData?.releases.length) {
+    courseSlug = userData.releases[0].course.slug;
+    const url = `https://www.filadd.com.br/courses/${courseSlug}/content`;
+
+    window.location.href = url;
+    window.document.body.innerHTML =
+      '<div id="redirect_message"><div>↗️ Redirecionando para seu curso...</div></div>';
+    return;
+  }
+
+  displayAuthButtons(userData);
+});
