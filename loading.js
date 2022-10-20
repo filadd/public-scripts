d2f72e2dc79b354ef756cb35200fd824fc8db1e3
Fran-Gonzalez <fran.gonzalez1994@gmail.com>
2022-10-20 16:18:06 -0300

    Create loading screen

---
+++ loading.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundColor = "#0050bd";
+document.body.innerHTML = `<div>
+    <div class="loading_logo">
+    <img
+        src="https://static.filadd.com/new-identity/logo/filadd_negative.svg"
+        alt="Academia Filadd"
+    />
+    <div class="ll_spinner"></div>
+    </div>
+    <div class="ll_text">
+    Redirecionando para seu curso...
+    </div>
+</div>`;
